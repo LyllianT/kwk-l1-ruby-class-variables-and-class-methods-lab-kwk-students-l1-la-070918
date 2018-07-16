923ee5a1bf1951fca25651e680b7a7448a9807ee
LyllianT <lilyterrific2008@gmail.com>
2018-07-16 19:11:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,11 @@
 class Song
   attr_accessor :name, :genre, :artist, 
   def initialize(name, genre, artist)
+    @name = name
     
+    @genre = genre
+    
+    @artist = artist
+  end
+end
+ninety_nine_problems = Song.new    
